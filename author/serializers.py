292f4260a4864bf6cfd979e05c611154fd8508a2
Anton Tsikhanovich <alluspeh@gmail.com>
2026-06-14 15:29:18 +0200

    Solution

--- author/serializers.py
+++ author/serializers.py
@@ -4,11 +4,13 @@ from author.models import Author
 
 
 class AuthorSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
+    id = serializers.IntegerField(read_only=True)  # noqa: VNE003
     first_name = serializers.CharField(max_length=64)
     last_name = serializers.CharField(max_length=64)
-    pseudonym = serializers.CharField(max_length=64, allow_null=True, required=False)
-    age = serializers.IntegerField()
+    pseudonym = serializers.CharField(
+        max_length=64,
+        allow_null=True,
+        required=False)
     age = serializers.IntegerField()
     retired = serializers.BooleanField()
 
